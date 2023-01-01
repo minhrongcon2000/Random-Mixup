8477911833bed4a903aab841848ad1c03c82a22b
John Pham <john@djavafactory.com>
2023-01-01 17:17:47 +0700

    Update policy network

--- policy_network.py
+++ policy_network.py
@@ -50,13 +50,58 @@ class CNNExtractor(BaseFeaturesExtractor):
         logits = self.logits_layer(observations["logits"])
         cat = torch.cat((saliency, logits), dim=1)
         return self.linear(cat)
+    
+
+class SaliencyGuidedRLCNNExtractor(BaseFeaturesExtractor):
+    def __init__(self, 
+                 observation_space: gym.spaces.Dict, 
+                 features_dim: int = 256):
+        super(SaliencyGuidedRLCNNExtractor, self).__init__(observation_space, features_dim)
+        n_input_channels = observation_space["original_saliency"].shape[0]
+        
+        self.original_cnn = nn.Sequential(
+            nn.Conv2d(
+                n_input_channels, 150, kernel_size=(3, 3), stride=(1, 1), padding=0
+            ),
+            nn.ReLU(),
+            nn.Conv2d(150, 100, kernel_size=(2, 2), stride=(1, 1), padding=0),
+            nn.ReLU(),
+            nn.Flatten(start_dim=1),
+        )
+        
+        self.perm_cnn = nn.Sequential(
+            nn.Conv2d(
+                n_input_channels, 150, kernel_size=(3, 3), stride=(1, 1), padding=0
+            ),
+            nn.ReLU(),
+            nn.Conv2d(150, 100, kernel_size=(2, 2), stride=(1, 1), padding=0),
+            nn.ReLU(),
+            nn.Flatten(start_dim=1),
+        )
+        
+        with torch.no_grad():
+            embed_dim = self.original_cnn(
+                torch.as_tensor(observation_space["original_saliency"].sample()[None]).float()
+            ).shape[-1]
+            
+            embed_dim += self.perm_cnn(
+                torch.as_tensor(observation_space["perm_saliency"].sample()[None]).float()
+            ).shape[-1]
+            
+        self.linear = nn.Sequential(nn.Linear(embed_dim, features_dim), nn.ReLU())
+        
+    def forward(self, observations):
+        original_saliency = self.original_cnn(observations["original_saliency"])
+        perm_saliency = self.perm_cnn(observations["perm_saliency"])
+        features = torch.cat([original_saliency, perm_saliency], dim=1)
+        return self.linear(features)
 
 
 if __name__ == "__main__":
     patch_size = 4
-    c = CNNExtractor(gym.spaces.Dict(dict(
-            saliency=gym.spaces.Box(0.0, 1.0, shape=(100, patch_size, patch_size)),
-            logits=gym.spaces.Box(-np.inf, np.inf, shape=(100, 100)),
+    c = SaliencyGuidedRLCNNExtractor(gym.spaces.Dict(dict(
+            original_saliency=gym.spaces.Box(0.0, 1.0, shape=(100, patch_size, patch_size)),
+            perm_saliency=gym.spaces.Box(0.0, 1.0, shape=(100, patch_size, patch_size)),
         )))
-    print(c(dict(saliency=torch.rand(1, 100, patch_size, patch_size),
-                 logits=torch.rand(1, 100, 100))))
+    print(c(dict(original_saliency=torch.rand(1, 100, patch_size, patch_size),
+                 perm_saliency=torch.rand(1, 100, patch_size, patch_size))))
