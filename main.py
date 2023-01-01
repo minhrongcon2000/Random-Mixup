6851ff5d1466d743a4531b33ecc1a50cd95cc06a
John Pham <john@djavafactory.com>
2023-01-01 18:01:56 +0700

    Add log env

--- main.py
+++ main.py
@@ -148,6 +148,7 @@ def main():
     print(f"The following configurations are set: {vars(args), config.items()}")
     if args.method == "rlmix":
         env = getattr(environment, args.env)(**ENV_config)
+        print(env)
         env = Monitor(env, "log" + str(args.seed))
         print("Running RL method.")
         logger = configure(
