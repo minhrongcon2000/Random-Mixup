Fix reward calculation

environment.py
@@ -519,7 +519,8 @@ class SaliencyGuidedRLMix(VanillaMixupPatchDiscrete):
         action = action.reshape(1, self.args.num_patches, self.args.num_patches)
         action = torch.as_tensor(action, dtype=torch.float32, device="cuda")
         lam = F.interpolate(action.unsqueeze(0), scale_factor=self.patch_size).squeeze(0)
-        _, mixup_image, _, loss = self.train_model(lam)
+        _, mixup_image, mixup_labels, loss = self.train_model(lam)
+        mixup_saliency = self.compute_saliency(mixup_image, mixup_labels, self.model, self.patch_size)
         
         # Compute gradients and do backprop
         self.optimizer.zero_grad()
@@ -529,7 +530,7 @@ class SaliencyGuidedRLMix(VanillaMixupPatchDiscrete):
             if self.args.scheduler == "OneCycleLR":
                 self.scheduler.step()
                 
-        reward = self.grad_sim(self.original_saliency, mixup_image)
+        reward = self.grad_sim(self.current_origin, mixup_saliency)
                 
         # Load the next batch
         try: